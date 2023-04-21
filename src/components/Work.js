ca2d5fd98a6d3c3137fd2471c9df6689a085659c
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 21:24:08 -0400

    add imports

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import PickMeals from "../Assets/pic-meals-image.png";
+import ChooseMeals from "../Assets/choose-image.png"
+import DeliveryMeals from "../Assets/dellivery-image.png"
 
 const Work = () => {
   return (
