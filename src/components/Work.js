8ae206aa6221e70fb47cbf327f052e71cbc3d28e
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 21:42:45 -0400

    data info addition

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,31 @@
 import React from "react";
-import PickMeals from "../Assets/pic-meals-image.png";
+import PickMeals from "../Assets/pick-meals-image.png";
 import ChooseMeals from "../Assets/choose-image.png"
-import DeliveryMeals from "../Assets/dellivery-image.png"
+import DeliveryMeals from "../Assets/delivery-image.png"
 
 const Work = () => {
-  return (
-    <div>Work</div>
-  )
+    const workInfoData = [
+        {
+            image: PickMeals,
+            title: "Pick Meals",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        },
+        {
+            image: ChooseMeals,
+            title: "Choose How Often",
+            text: "Lorem ipsum dolor sit amet, consectetur ",
+        },
+        {
+            image: DeliveryMeals,
+            title: "Fast Deliveries",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        },
+    ];
+
+
+
+
+  return <div>Work</div>
 }
 
 export default Work;
