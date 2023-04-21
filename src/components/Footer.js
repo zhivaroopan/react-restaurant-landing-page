1a89fb37d4602108d4050a0a7b527893da93a657
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 22:48:02 -0400

    footer imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import React from "react"
+import Logo from "../Assets/Logo.svg";
+import {BsTwitter} from "react-icons/bs";
+import {SiLinkedin} from "react-icons/si";
+import {BsYoutube} from "react-icons/bs";
+import {FaFacebook} from "react-icons/fa";
 
 const Footer = () => {
   return (
