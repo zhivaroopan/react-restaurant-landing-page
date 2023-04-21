d7a134a095bf1138518cd02d62192643379275fb
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 21:55:44 -0400

    work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -25,7 +25,30 @@ const Work = () => {
 
 
 
-  return <div>Work</div>
+  return <div className="work-section-wrapper">
+            <div className="work-section-top">
+                <p className="primary-subheading">Work</p>
+                <h1 className="primary-subheading">How It Works</h1>
+                <p className="primary-text">
+                accumsan lacus vel. Praesent elementum facilisis leo vel fringilla est. Ut porttitor leo a diam.
+                 Nibh mauris cursus mattis molestie a. Ac tortor vitae
+                </p>
+            </div>
+            <div className="work-section-bottom" >
+                {
+                    workInfoData.map((data) => (
+                       <div className="work-section-info">
+                            <div className="info-boxes-img-container">
+                                <img src={data.image} alt=""/>
+                            </div>
+                            <h2>{data.title}</h2>
+                            <p>{data.text}</p>
+                       </div>
+                    ) )
+                }
+
+            </div>
+  </div>
 }
 
 export default Work;
