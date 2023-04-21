5b24c36ba318dda449eba709005fb734d9c5d382
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 21:42:26 -0400

    removal

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 
+
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      
     </div>
   );
 }
