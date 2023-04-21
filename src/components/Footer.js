e4022cd465a76dda3834e371a7de1fdd47a26bd7
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 23:07:28 -0400

    footer addition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Logo from "../Assets/Logo.svg";
 import {BsTwitter} from "react-icons/bs";
 import {SiLinkedin} from "react-icons/si";
 import {BsYoutube} from "react-icons/bs";
-import {FaFacebook} from "react-icons/fa";
+import {FaFacebookF} from "react-icons/fa";
 
 const Footer = () => {
   return <div className="footer-wrapper">
@@ -11,6 +11,32 @@ const Footer = () => {
                 <div className="footer-logo-container">
                     <img src={Logo} alt="Website Logo"/>
                 </div>
+                <div className="footer-icons">
+                    <BsTwitter />
+                    <SiLinkedin />
+                    <BsYoutube/>
+                    <FaFacebookF />
+                </div>
+            </div>
+            <div className="footer-section-two">
+                <div className="footer-section-columns">
+                    <span>Quality</span>
+                    <span>Help</span>
+                    <span>Share</span>
+                    <span>Carrers</span>
+                    <span>Testimonials</span>
+                    <span>Work</span>
+                </div>
+                <div className="footer-section-columns">
+                    <span>800-543-3982</span>
+                    <span>hello@foodie.com</span>
+                    <span>press@foodie.com</span>
+                    <span>contact@foodie.com</span>
+                </div>
+                <div className="footer-section-columns">
+                <span>Terms & Conditions</span>
+                <span>Privacy Policy</span>
+            </div>
             </div>
   </div>;  
 };
