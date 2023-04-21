e78dfaab9cf0023184852cf769915b4d37ac17a1
quisethecoder <marquise_dawkins@aol.com>
2023-04-20 20:53:31 -0400

    add import

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import {BsFillPlayCircleFill} from "react-icons/bs";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div className="about-section-container">
+            <div className="about-background-image-container">
+                <img src={AboutBackground} alt=""/>
+            </div>
+            <div className="about-section-image-container">
+                <img src={AboutBackgroundImage} alt=""/>
+            </div>
+  </div>;
+};
 
 export default About;
